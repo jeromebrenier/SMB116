03581c9d9b8f82cede01eca865436a8f4d2230f8
Jérôme Brenier <j.brenier@outlook.fr>
2019-12-16 20:14:42 +0100

    wip

---
+++ Petfoodingcontrol/app/src/main/java/fr/jbrenier/petfoodingcontrol/ui/fragments/LoginWelcomeViewModel.java
@@ -0,0 +1,7 @@
+package fr.jbrenier.petfoodingcontrol.ui.fragments;
+
+import androidx.lifecycle.ViewModel;
+
+public class LoginWelcomeViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
